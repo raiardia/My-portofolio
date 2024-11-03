94aa814697a017aec44e581635283b7f37436caa
raiardia <ardiaanggreni@gmail.com>
2024-11-03 10:20:59 +0800

    menambahkan desktop layout

---
+++ my_portofolio/lib/screens/largescreen/desktop_layout_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class DesktopLayout extends StatefulWidget {
+  const DesktopLayout({super.key});
+
+  @override
+  State<DesktopLayout> createState() => _DesktopLayoutState();
+}
+
+class DesktopLayoutPage extends State<DesktopLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+
+    );
+  }
+}
